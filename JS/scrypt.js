c78e64070855f3f4f87d1a662094bd76bd45df1d
alexandrejunior2923-ship-it <alexandrejuior2923@gmial.com>
2025-11-25 11:57:02 -0300

    versao 0.1

--- JS/scrypt.js
+++ JS/scrypt.js
@@ -81,3 +81,18 @@ document.addEventListener("DOMContentLoaded", () => {
   btn.setAttribute("target", "_blank");
   btn.setAttribute("rel", "noopener noreferrer");
 });
+//depoimentos
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
+
+function showSlide(index) {
+    slides.forEach(s => s.classList.remove("active"));
+    dots.forEach(d => d.classList.remove("active"));
+
+    slides[index].classList.add("active");
+    dots[index].classList.add("active");
+}
+
+dots.forEach((dot, index) => {
+    dot.addEventListener("click", () => showSlide(index));
+});
